443cafec5e523d7636fc709d4a0ee44755c8a949
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-02-26 16:01:56 +0100

    feat: add integrity plugin

---
+++ src/Plugin.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class Plugin {
+	constructor(filename = "integrity.json") {
+		if (typeof filename !== "string") {
+			throw new Error("The filename should be a String");
+		}
+		this.filename = filename;
+	}
+	apply(compiler) {
+		compiler.hooks.emit.tap("wildpeaks-integrity-plugin", compilation => {
+			const aggregated = {};
+			const {assets} = compilation;
+			const ids = Object.keys(assets).sort();
+			for (const id of ids) {
+				const {integrity} = assets[id];
+				if (typeof integrity === "string") {
+					aggregated[id] = integrity;
+				}
+			}
+			const buffer = JSON.stringify(aggregated);
+			assets[this.filename] = {
+				source() {
+					return buffer;
+				},
+				size() {
+					return buffer.length;
+				}
+			};
+		});
+	}
+}
+
+module.exports = Plugin;
